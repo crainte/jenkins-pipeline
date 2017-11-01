d0cb8cf9ac6faee4788db5bcb8ae9b643c61e64b
Chris Alexander <chris.alexander@rackspace.com>
2017-11-01 11:02:49 -0500

    Add githubStatus to generate commit check statuses

---
+++ vars/githubStatus.groovy
@@ -0,0 +1,12 @@
+def call(String context, String message, String state) {
+    import groovy.json.JsonOutput
+
+    display = context.replaceAll(" ", "-")
+    fullcontext = "continuous-integration/jenkins/${display}"
+
+    withCredentials([[$class: 'StringBinding', credentialsId: 'GITHUB_TOKEN', variable: 'GITHUB_TOKEN']]) {
+        def payload = JsonOutput.toJson(["state": "${state}", "description":"${message}", "context": "${fullcontext}", "target_url": "${env.BUILD_URL}"])
+        def apiURL = "${env.GITHUB_API}/repos/${env.GIT_REMOTE_ORIGIN}/statuses/${env.GIT_COMMIT}"
+        sh """curl -s -H "Authorization: Token ${env.GITHUB_TOKEN}" -H "Accept: application/json" -H "Content-type: application/json" -X POST -d'${payload}' ${apiURL}"""
+    }
+}
