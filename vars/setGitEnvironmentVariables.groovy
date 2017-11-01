d1b2d50d0c71c98abb0f4f37a823707575b2f7fb
Chris Alexander <chris.alexander@rackspace.com>
2017-11-01 10:56:40 -0500

    Update setGitEnvVar to support remote origin

--- vars/setGitEnvironmentVariables.groovy
+++ vars/setGitEnvironmentVariables.groovy
@@ -2,4 +2,5 @@ def call() {
   env.GIT_COMMIT = sh([returnStdout: true, script: 'git rev-parse HEAD']).trim()
   env.GIT_COMMIT_SHORT = env.GIT_COMMIT.take(6)
   env.GIT_BRANCH = getGitBranch()
+  env.GIT_REMOTE_ORIGIN = getGitRemoteOrigin()
 }
