2ae4516a5e1b417e94a90fcf3c5273085d5958ff
Chris Alexander <chris.alexander@rackspace.com>
2017-11-02 11:19:17 -0500

    Skip file diff on empty target

--- vars/getGitChangedFiles.groovy
+++ vars/getGitChangedFiles.groovy
@@ -1,4 +1,8 @@
 def call() {
+    if ( env.CHANGE_TARGET == null ) {
+        println "Changed files skipped. No target found."
+        return
+    }
     def cmd = "git diff --diff-filter=d --name-only remotes/origin/${env.CHANGE_TARGET}..HEAD"
     def files = sh(returnStdout: true, script: cmd).trim()
     return files
