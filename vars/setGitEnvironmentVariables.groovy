b32168fb30bf25aab7e9808b3bca424a0ff6f8e9
Chris Alexander <chris.alexander@rackspace.com>
2018-02-22 16:32:20 -0600

    Behave differently on PR's

--- vars/setGitEnvironmentVariables.groovy
+++ vars/setGitEnvironmentVariables.groovy
@@ -1,7 +1,11 @@
 def call() {
-  env.GIT_COMMIT = sh([returnStdout: true, script: 'git rev-parse ORIG_HEAD']).trim()
-  env.GIT_COMMIT_SHORT = env.GIT_COMMIT.take(6)
   env.GIT_BRANCH = getGitBranch()
+  if (env.BRANCH_NAME.startsWith('PR-')) {
+    env.GIT_COMMIT = sh([returnStdout: true, script: 'git rev-parse ORIG_HEAD']).trim()
+  } else {
+    env.GIT_COMMIT = sh([returnStdout: true, script: 'git rev-parse HEAD']).trim()
+  }
+  env.GIT_COMMIT_SHORT = env.GIT_COMMIT.take(6)
   env.GIT_REMOTE_ORIGIN = getGitRemoteOrigin()
   env.GIT_CHANGED_FILES = getGitChangedFiles()
 }
